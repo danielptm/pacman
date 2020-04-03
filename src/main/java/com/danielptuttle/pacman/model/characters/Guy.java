9028ffa914b294f2c0b7591b7c6bb4f67f51c07d
Daniel <danielptm@me.com>
2020-04-02 22:21:46 -0700

    Got pacman able to move properly according to the direction

--- src/main/java/com/danielptuttle/pacman/model/characters/Guy.java
+++ src/main/java/com/danielptuttle/pacman/model/characters/Guy.java
@@ -10,6 +10,11 @@ public abstract class Guy {
     private int currentPositionX;
     private int currentPositionY;
 
+    int upCounter = 9;
+    int rightCounter = 0;
+    int downCounter = 3;
+    int leftCounter = 6;
+
     private Image currentState;
 
     protected Image[] physicalStates;
@@ -37,6 +42,11 @@ public abstract class Guy {
     void calculateImageState(int prevX, int prevY, int newX, int newY) {
         if (prevX == newX) {
             if (newY > prevY) {
+                if (downCounter < 5) {
+                    downCounter++;
+                } else {
+                    downCounter = 3;
+                }
                 guyDirection = GuyDirection.DOWN;
             }
             if (newY < prevY) {
@@ -45,9 +55,19 @@ public abstract class Guy {
         }
         if (prevY == newY) {
             if (newX > prevX) {
+                if (rightCounter < 2) {
+                    rightCounter++;
+                } else {
+                    rightCounter = 0;
+                }
                 guyDirection = GuyDirection.RIGHT;
             }
             if (newX < prevX) {
+                if (leftCounter < 8) {
+                    leftCounter++;
+                } else {
+                    leftCounter = 6;
+                }
                 guyDirection = GuyDirection.LEFT;
             }
         }
@@ -56,16 +76,16 @@ public abstract class Guy {
     public Image getCurrentState() {
         switch (this.guyDirection) {
             case UP:
-                this.currentState = physicalStates[9];
+                this.currentState = physicalStates[upCounter];
                 break;
             case RIGHT:
-                this.currentState = physicalStates[1];
+                this.currentState = physicalStates[rightCounter];
                 break;
             case DOWN:
-                this.currentState = physicalStates[4];
+                this.currentState = physicalStates[downCounter];
                 break;
             case LEFT:
-                this.currentState = physicalStates[7];
+                this.currentState = physicalStates[leftCounter];
                 break;
         }
         return currentState;
